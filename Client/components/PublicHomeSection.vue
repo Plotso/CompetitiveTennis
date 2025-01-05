d571cac436021dc6f3eb4fc512b0b725dca9f75e
Plotso <ilian_1999n@abv.bg>
2025-01-05 18:56:28 +0200

    Public page now includes 6 points that are aiming to grab the intention of guest users to potentially trigger them to join the platform.

--- Client/components/PublicHomeSection.vue
+++ Client/components/PublicHomeSection.vue
@@ -1,27 +1,51 @@
 <script setup lang="ts">
+//ToDo: SEO optimise this page
 const points = [
-        {
-          id: 1,
-          logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
-          description: 'Description for Point 1',
-        },
-        {
-          id: 2,
-          logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
-          description: 'Description for Point 2',
-        },
-        {
-          id: 3,
-          logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
-          description: 'Description for Point 1',
-        },
-        {
-          id: 4,
-          logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
-          description: 'Description for Point 2',
-        },
-        // Add more points as needed
-      ]
+  {
+    id: 1,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-globe',
+    title: 'Be Part of Something Bigger',
+    description: 'Join a platform that aims to grow the sport of tennis, foster community engagement, and create opportunities for players of all ages and skill levels.',
+  },
+  {
+    id: 2,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-users',
+    title: 'Community and Camaraderie',
+    description: 'Easily find and connect with tennis enthusiasts in your area, arrange matches, and build lasting friendships within a vibrant tennis community.',
+  },
+  {
+    id: 3,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-calendar-alt',
+    title: 'Seamless Tournament Management',
+    description: 'Effortlessly join or create tournaments, manage registrations, and track results through our user-friendly platform.',
+  },
+  {
+    id: 4,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-trophy',
+    title: 'Fair and Innovative Draw System',
+    description: 'Compete with confidence in tournaments with a fair draw system. Our rating-based draw generation ensures exciting and balanced matches for everyone.',
+  },
+  {
+    id: 5,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-chart-line',
+    title: 'Build Your Tennis Profile',
+    description: 'Track your progress with a dynamic player profile. Showcase your skills, climb the rankings, and watch your tennis journey unfold.',
+  },
+  {
+    id: 6,
+    logo: 'https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000',
+    fa_logo: 'fa-solid fa-heart-pulse',
+    title: 'Promote an Active Lifestyle & Boost Your Sport Enthusiasm',
+    description: 'Engage in regular physical activity, improve your health, and experience the numerous physical and mental benefits of competitive tennis. Turn your passion into a lifestyle while having fun and staying fit.',
+  },
+];
+
+
 </script>
 
 <template>
@@ -30,8 +54,8 @@ const points = [
         <div class="columns is-centered is-multiline">
           <div class="column is-4" v-for="point in points" :key="point.id">
             <div class="point">
-              <div class="point-logo">
-                <font-awesome-icon icon="fa-solid fa-check" />
+              <div class="point-logo is-large">
+                <font-awesome-icon :icon="point.fa_logo" /> {{ point.title }}
               </div>
               <div class="point-description">
                 <p>{{ point.description }}</p>
